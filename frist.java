933b8af50528ed20919f0623fea9f2558b9b2f80
nitinmakin <67549072+nitinmakin@users.noreply.github.com>
2020-06-29 21:21:06 +0530

    Update frist.java

--- frist.java
+++ frist.java
@@ -2,6 +2,6 @@ class frist
 {
 public static void main(String[] args)
 {
-System.out.println("frist project update in feature branch");
+System.out.println("frist project update in feature branch, edited in remote");
 }
 }
